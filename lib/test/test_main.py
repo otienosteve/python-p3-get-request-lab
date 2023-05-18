36436fdb436327a9b3af77462347f57541f9a9a1
otienosteve <steveotieno701@gmail.com>
2023-05-19 00:56:48 +0300

    Ad test for route

--- lib/test/test_main.py
+++ lib/test/test_main.py
@@ -2,21 +2,27 @@ import pytest
 from ..main import app, Employee
 from fastapi.testclient import TestClient
 from fastapi import status
+from ..models import Employee, session
 
+@pytest.fixture(scope='session')
+def data() -> None:
+    # return session.query(Employee).all()
+    return "okay"
 
 @pytest.fixture(scope='session')
 def client():
     return TestClient(app)
 
 def test_schema()-> None:
-    emp = Employee(first_name="Donald", last_name='Knuth', age=24, salary=43000, phone_number=254712345678, email='donaldknuth@gmail.com', designation='It Dept')
+    pass
+    # emp = Employee(first_name="Donald", last_name='Knuth', age=24, salary=43000, phone_number=254712345678, email='donaldknuth@gmail.com', designation='It Dept')
     # last_name , firs_tname, email, age, phone_number, salary, designation
 
-def test_root(client: TestClient):
+def test_root(client: TestClient, data: data):
     res = client.get('/')
     assert res.status_code == status.HTTP_200_OK, "unsuccesful request"
-    assert res.json() == {"message" : "Welcome to Kenya"} , "Unexpected json response"
-
+    assert res.json() == data
+    
     
 def test_students(client: TestClient):
     res = client.get('/students')
